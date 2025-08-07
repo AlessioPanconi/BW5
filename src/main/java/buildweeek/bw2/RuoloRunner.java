83c762453f03a76779176faa1d7113697f3d1a9e
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-08-07 12:32:33 +0200

    variazione nella creazione admin

--- src/main/java/buildweeek/bw2/RuoloRunner.java
+++ src/main/java/buildweeek/bw2/RuoloRunner.java
@@ -6,6 +6,7 @@ import buildweeek.bw2.entities.Utente;
 import buildweeek.bw2.repositories.RuoloRepository;
 import buildweeek.bw2.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,17 @@ public class RuoloRunner implements CommandLineRunner {
     @Autowired
     private UtenteService utenteService;
 
+    @Value("${admin.username}")
+    private String adminUsername;
+    @Value("${admin.email}")
+    private String adminEmail;
+    @Value("${admin.password}")
+    private String adminPassword;
+    @Value("${admin.nome}")
+    private String adminNome;
+    @Value("${admin.cognome}")
+    private String adminCognome;
+
     @Override
     public void run(String... args) throws Exception {
         if (ruoloRepository.count() == 0) {
@@ -26,11 +38,11 @@ public class RuoloRunner implements CommandLineRunner {
         }
         try {
             NewUtenteDTO adminDto = new NewUtenteDTO(
-                    "admin",
-                    "emailAdmin@gmail.com",
-                    "Admin123!",
-                    "Mario",
-                    "Rossi"
+                    adminUsername,
+                    adminEmail,
+                    adminPassword,
+                    adminNome,
+                    adminCognome
             );
             Utente admin = utenteService.saveUtente(adminDto);
             utenteService.findUtenteByIdAndPatchRuolo(admin.getIdUtente());
