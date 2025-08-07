af1d10affdad90022d245d592900791e579fad96
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-08-07 11:51:44 +0200

    inizializzazione admin all'avvio

--- src/main/java/buildweeek/bw2/RuoloRunner.java
+++ src/main/java/buildweeek/bw2/RuoloRunner.java
@@ -1,7 +1,10 @@
 package buildweeek.bw2;
 
+import buildweeek.bw2.DTO.NewUtenteDTO;
 import buildweeek.bw2.entities.Ruolo;
+import buildweeek.bw2.entities.Utente;
 import buildweeek.bw2.repositories.RuoloRepository;
+import buildweeek.bw2.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,6 +14,8 @@ public class RuoloRunner implements CommandLineRunner {
 
     @Autowired
     private RuoloRepository ruoloRepository;
+    @Autowired
+    private UtenteService utenteService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -19,5 +24,21 @@ public class RuoloRunner implements CommandLineRunner {
             ruoloRepository.save(new Ruolo("ADMIN"));
             System.out.println("Ruoli caricati nel database");
         }
+        try {
+            NewUtenteDTO adminDto = new NewUtenteDTO(
+                    "admin",
+                    "emailAdmin@gmail.com",
+                    "Admin123!",
+                    "Mario",
+                    "Rossi"
+            );
+            Utente admin = utenteService.saveUtente(adminDto);
+            utenteService.findUtenteByIdAndPatchRuolo(admin.getIdUtente());
+            System.out.println("L'utente admin è stato creato correttamente✅");
+        } catch (Exception e) {
+            System.out.println("L'utente admin esiste già!");
+        }
     }
+
+
 }
